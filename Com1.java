d628daaa8d71225d784f513eeaae69520d123120
arpitgpta <55844150+arpitgpta@users.noreply.github.com>
2019-10-16 22:59:57 +0530

    initial commit

---
+++ Com1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+
+public class Com1{
+  public static void main(String[] args) throws Exception{
+        try{
+            ServerSocket ss = new ServerSocket(7);
+            Socket s = ss.accept();
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            String str = (String)din.readUTF();
+            System.out.println(str);
+            do{
+                str = (String)din.readUTF();
+                System.out.println("client says: " + str);
+            }while(str.charAt(0) != '-');
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }  
+  
+}
